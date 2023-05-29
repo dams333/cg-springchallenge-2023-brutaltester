921385d2ae1cdbec3bfc9d4c0388ea1d76628848
dhubleur <dhubleur@student.42.fr>
2023-05-29 10:51:21 +0200

    script

--- run.sh
+++ run.sh
@@ -1,2 +1,19 @@
-java -jar brutaltester.jar -r "java -Dleague.level=$3 --add-opens java.base/java.lang=ALL-UNNAMED -jar referee.jar" -p1 "$1" -p2 "$2"
+if [ "$#" -lt 3 ]; then
+	echo "Usage: sh run_referee.sh [CMD PLAYER1] [CMD PLAYER 2] [LEAGUE LEVEL] {GAMES, default 1} {threads, default 1}"
+	echo "CMD needs to be runnable here, likes \"./a.out\" or \"python3 script.py\""
+	echo "League level is 1 for Wood2, 2 for Wood1, 3 for Bronze"
+	exit $1
+fi
 
+games=1
+if [ "$#" -gt 3 ]; then
+	games=$4
+fi
+
+threads=1
+if [ "$#" -gt 4 ]; then
+	threads=$5
+fi
+
+
+java -jar brutaltester.jar -r "java -Dleague.level=$3 --add-opens java.base/java.lang=ALL-UNNAMED -jar referee.jar" -p1 "$1" -p2 "$2" -n $games -t $threads
--- run_referee.sh
+++ run_referee.sh
@@ -1,1 +1,8 @@
+if [ "$#" -ne 3 ]; then
+	echo "Usage: sh run_referee.sh [CMD PLAYER1] [CMD PLAYER 2] [LEAGUE LEVEL]"
+	echo "CMD needs to be runnable here, likes \"./a.out\" or \"python3 script.py\""
+	echo "League level is 1 for Wood2, 2 for Wood1, 3 for Bronze"
+	exit 1
+fi
+
 java -Dleague.level=$3 --add-opens java.base/java.lang=ALL-UNNAMED -jar referee.jar -p1 "$1" -p2 "$2"
